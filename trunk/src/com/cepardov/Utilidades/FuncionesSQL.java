c7018f9957a61a0b3d87275d36aed25bdbf4304a
cepardov@gmail.com <cepardov@gmail.com@71812de5-e15c-bc1a-c891-a4679620f04f>
2013-11-20 19:30:59 +0000

    Se Agrega un servicio de inicio de sesion + configuraciones iniciales Correccion de errores

--- trunk/src/com/cepardov/Utilidades/FuncionesSQL.java
+++ trunk/src/com/cepardov/Utilidades/FuncionesSQL.java
@@ -20,6 +20,51 @@ public class FuncionesSQL {
         conn = new Conect();
     }
     
+    public int getUsuario(){
+        int posid = 0;
+        try{
+            PreparedStatement pstm = conn.getConnection().prepareStatement("SELECT count(1) as total FROM Usuario");
+            ResultSet res = pstm.executeQuery();
+            res.next();
+            posid = res.getInt("total");
+            res.close();
+            }catch(SQLException e){
+                JOptionPane.showMessageDialog(null, e);
+        }
+        return posid;
+    }
+    
+    public void updateClave(String clave, String usuario){
+       try {                
+            PreparedStatement pstm = conn.getConnection().prepareStatement("update Usuario "+
+            "set clave=?"+
+            "where usuario=?");            
+            pstm.setString(1, clave);                 
+            pstm.setString(2, usuario);
+            pstm.execute();
+            pstm.close();            
+       }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+      }
+   }
+    
+    public void addUsuario(String rut, String nombre, String apellido, String usuario, String clave, String pregunta, String respuesta){
+        try{
+            PreparedStatement pstm = conn.getConnection().prepareStatement("insert into Usuario (rut,nombre,apellido,usuario,clave,pregunta,respuesta) values(?,?,?,?,?,?,?)");
+            pstm.setString(1, rut);
+            pstm.setString(2, nombre);
+            pstm.setString(3, apellido);
+            pstm.setString(4, usuario);
+            pstm.setString(5, clave);
+            pstm.setString(6, pregunta);
+            pstm.setString(7, respuesta);
+            pstm.execute();
+            pstm.close();
+            }catch(SQLException e){
+                JOptionPane.showMessageDialog(null, "Revise los datos ingresados no se ha podido guardar la solicitud.\nContacte con Administrador de la aplicación.\n"+e);
+        }
+    }
+    
     public void addMarca(String nombre){
         try{
             PreparedStatement pstm = conn.getConnection().prepareStatement("insert into Marca (nombre) values(?)");
@@ -45,6 +90,58 @@ public class FuncionesSQL {
                 JOptionPane.showMessageDialog(null, e);
         }
     }
+    
+    public void addCliente(String rutCliente, String nombre, String paterno, String materno, String Estado, String direccion, String ciudad, String telefono, String email, String fechaIngreso){
+        try{
+            PreparedStatement pstm = conn.getConnection().prepareStatement("insert into Cliente (rutCliente,nombre,paterno,materno,estado,direccion, ciudad,telefono,email,fechaIngreso) values(?,?,?,?,?,?,?,?,?,?)");
+            pstm.setString(1, rutCliente);
+            pstm.setString(2, nombre);
+            pstm.setString(3, paterno);
+            pstm.setString(4, materno);
+            pstm.setString(5, Estado);
+            pstm.setString(6, direccion);
+            pstm.setString(7, ciudad);
+            pstm.setString(8, telefono);
+            pstm.setString(9, email);
+            pstm.setString(10, fechaIngreso);
+            pstm.execute();
+            pstm.close();
+            JOptionPane.showMessageDialog(null, "Operacion realizada...");
+            }catch(SQLException e){
+                JOptionPane.showMessageDialog(null, "No se ha podido procesar su solicitud por alguna de estas razónes:\n\t- El cliente ya ha sido ingresado(a).\n\t- Si ha modificado la ficha del cliente presione boton \"Corregir\".", "¡ups! Algo salio mal durante el almacenado de datos...", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    public void updateCliente(String rutCliente, String nombre, String paterno, String materno, String Estado, String direccion, String ciudad, String telefono, String email){
+       try {            
+            PreparedStatement pstm = conn.getConnection().prepareStatement("update Cliente " +
+            "set rutCliente = ? ," +
+            "nombre = ? ," +
+            "paterno = ? ," +
+            "materno = ? ," +
+            "estado = ? ," + 
+            "direccion = ? ," +
+            "ciudad = ? ," +
+            "telefono = ? ," + 
+            "email = ? " +
+            "where rutCliente = ?");            
+            pstm.setString(1, rutCliente);
+            pstm.setString(2, nombre);
+            pstm.setString(3, paterno);
+            pstm.setString(4, materno);
+            pstm.setString(5, Estado);
+            pstm.setString(6, direccion);
+            pstm.setString(7, ciudad);
+            pstm.setString(8, telefono);
+            pstm.setString(9, email);
+            pstm.setString(10, rutCliente);
+            pstm.execute();
+            pstm.close();  
+            JOptionPane.showMessageDialog(null, "Trabajo realizado exitosamente.");
+         }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+      }
+   }
     
     public Object [][] getMarca(){
         int posid = 0;
@@ -109,4 +206,5 @@ public class FuncionesSQL {
         }
         return data;
     }
+    
 }
